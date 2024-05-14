95190f4f70f7b298331c58a823426f879476b261
Saiful182 <saifulislamcse0@gamil.com>
2024-05-14 05:04:42 +0200

    Error handled

--- js/deposite.js
+++ js/deposite.js
@@ -2,20 +2,27 @@ document.getElementById('btn-deposite').addEventListener('click', function () {
     const depositeField = document.getElementById('deposite-field');
     const newDepositeAmountString = depositeField.value;
     const newDepositeAmount = parseFloat(newDepositeAmountString);
+    if (isNaN(newDepositeAmount)) {
+        alert('Please Enter A Valid Number');
+        depositeField.value = '';
+    }
+    else {
+        const depositeTotalElement = document.getElementById('deposite-total');
+        const PreviousDepositeString = depositeTotalElement.innerText;
+        const PreviousDeposite = parseFloat(PreviousDepositeString);
+        const curentDepositeTotal = PreviousDeposite + newDepositeAmount;
 
-    const depositeTotalElement = document.getElementById('deposite-total');
-    const PreviousDepositeString = depositeTotalElement.innerText;
-    const PreviousDeposite = parseFloat(PreviousDepositeString);
-    const curentDepositeTotal = PreviousDeposite + newDepositeAmount;
+        depositeTotalElement.innerText = curentDepositeTotal;
+        const previousBalanceElement = document.getElementById('total-balance');
+        const previousBalanceString = previousBalanceElement.innerText;
+        const previousBalance = parseFloat(previousBalanceString);
 
-    depositeTotalElement.innerText = curentDepositeTotal;
-    const previousBalanceElement = document.getElementById('total-balance');
-    const previousBalanceString = previousBalanceElement.innerText;
-    const previousBalance = parseFloat(previousBalanceString);
-
-    const newTotalBalce = previousBalance + curentDepositeTotal;
-    previousBalanceElement.innerText = newTotalBalce;
+        const newTotalBalce = previousBalance + curentDepositeTotal;
+        previousBalanceElement.innerText = newTotalBalce;
 
 
-    depositeField.value = '';
+        depositeField.value = '';
+
+    }
+
 })
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,23 +2,40 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawAmountElement = document.getElementById('new-withdraw');
     const newWithdrawAmountString = newWithdrawAmountElement.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    if (isNaN(newWithdrawAmount)) {
+        newWithdrawAmountElement.value = '';
+        alert('Please Enter a valid Number');
+    }
+    else {
+        const previousBalanceElement = document.getElementById('total-balance');
+        const previousBalanceString = previousBalanceElement.innerText;
+        const previousBalance = parseFloat(previousBalanceString);
 
-    const previousWithdrawElement = document.getElementById('withdraw-total');
-    const previousWithdrawString = previousWithdrawElement.innerText;
-    const previousWithdraw = parseFloat(previousWithdrawString);
+        if (newWithdrawAmount > previousBalance) {
+            newWithdrawAmountElement.value = '';
+            alert("Insuffecient Balance");
+        }
+        else {
+            const previousWithdrawElement = document.getElementById('withdraw-total');
+            const previousWithdrawString = previousWithdrawElement.innerText;
+            const previousWithdraw = parseFloat(previousWithdrawString);
 
-    const totalWithdraw = previousWithdraw + newWithdrawAmount;
+            const totalWithdraw = previousWithdraw + newWithdrawAmount;
 
-    previousWithdrawElement.innerText = totalWithdraw;
+            previousWithdrawElement.innerText = totalWithdraw;
 
-    const previousBalanceElement = document.getElementById('total-balance');
-    const previousBalanceString = previousBalanceElement.innerText;
-    const previousBalance = parseFloat(previousBalanceString);
 
-    const newTotalBalce = previousBalance - newWithdrawAmount;
-    previousBalanceElement.innerText = newTotalBalce;
+            const newTotalBalce = previousBalance - newWithdrawAmount;
+            previousBalanceElement.innerText = newTotalBalce;
+            newWithdrawAmountElement.value = '';
 
-    newWithdrawAmountElement.value = '';
+        }
+
+
+
+    }
+
+
 
 
 })
